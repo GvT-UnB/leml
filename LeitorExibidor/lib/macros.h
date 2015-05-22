9b2932606ec2928cf634a1116ae792fb2e76e30a
diogoAF <diogo.assis.ferreira@gmail.com>
2015-05-21 23:14:12 -0300

    Definição de macros em macros.h

---
+++ LeitorExibidor/lib/macros.h
@@ -0,0 +1,123 @@
+/**< Assinatura do arquivo .class */
+#define U4_MAGIC 0xCAFEBABE
+
+/**< Máscara de bits abaixo que especifica permissões de acesso e propriedades da classe, interface, métodos e atributos */
+#define ACC_PUBLIC 0x0001 //Declaração pública para CLASSES, METODOS E ATRIBUTOS
+#define ACC_PRIVATE 0x0002 //Declaração privada para METODOS E ATRIBUTOS
+#define ACC_PROTECTED 0x0004 //Declaração protegida para METODOS E ATRIBUTOS
+#define ACC_STATIC 0x0008 //Declaração estática, ATRIBUTO pertence à classe, não à instância!
+#define ACC_FINAL 0x0010 //Declaração Final CLASSES, METODOS E ATRIBUTOS
+#define ACC_SYNCHRONIZED 0x0020 //Declaração sincronizado para MÉTODOS
+#define ACC_VOLATILE 0x0040 //Declaração volátil para ATRIBUTOS
+#define ACC_TRANSIENT 0x0080 //Declarada transiente: não pode ser lida ou gravada por um gerente de objetos persistente. (Reserva para uso futuro da Sun)
+#define ACC_NATIVE 0x0100 //MÉTODO Nativo: implementado em linguagem não Java (C, C++, Assembly)
+#define ACC_SUPER 0x0020 //Declaração Super, relativo à superclasse
+#define ACC_INTERFACE 0x0200 //Declaração de interface, não é uma classe
+#define ACC_ABSTRACT 0x0400 //Declaração abstrata, CLASSES não podem ser instanciadas e MÉTODOS não possuem definição.
+#define ACC_STRICT 0x0800 //MÉTODO Strictfp: utiliza modo de ponto flutuante FP-strict (não normalizado)
+
+/**< Tipos Válidos de Tags (Constants Pool) */
+#define CONSTANT_Class              7
+#define CONSTANT_Fieldref           9
+#define CONSTANT_Methodref          10
+#define CONSTANT_InterfaceMethodref 11
+#define CONSTANT_String             8
+#define CONSTANT_Integer            3
+#define CONSTANT_Float              4
+#define CONSTANT_Long               5
+#define CONSTANT_Double             6
+#define CONSTANT_NameAndType        12
+#define CONSTANT_Utf8               1
+
+/**<Tipos de Dados do arquivo .class*/
+typedef unsigned char u1; //1 byte
+typedef unsigned short u2; //2 bytes
+typedef unsigned int u4; //4 bytes
+
+/**< O famoso Constante Pool! */
+typedef struct {
+	u1 tag;
+	union {
+   		struct {
+      		u2 name_index;
+		} Class; //tag = 7
+		struct {
+            u2 class_index;
+            u2 name_and_type_index;
+		} Fieldref; //tag = 9
+		struct {
+            u2 name_index;
+            u2 descriptor_index;
+		} NameAndType; //tag = 12
+		struct {
+            u2 length;
+            //u1 bytes[length];
+            u1 *bytes;
+		}UTF8; //tag = 1
+		struct {
+            u2 class_index;
+            u2 name_and_type_index;
+		} Methodref; //tag = 10
+		struct {
+            u2 class_index;
+            u2 name_and_type_index;
+		} InterfaceMethodref; //tag = 11
+		struct {
+            u2 string_index;
+		} String; //tag = 8
+		struct {
+            u4 bytes;
+		} Integer; //tag = 3
+		struct {
+            u4 bytes;
+		} Float; //tag = 4
+		struct {
+            u4 high_bytes;
+            u4 low_bytes;
+		} Long; //tag = 5
+		struct {
+            u4 high_bytes;
+            u4 low_bytes;
+		} Double; //tag = 6
+	}
+}cp_info;
+
+/**<Estrutura Interna dum arquivo .class*/
+typedef struct{
+    u4 magic;
+    u2 minor_version;
+    u2 major_version;
+    u2 constant_pool_count;
+    //cp_info constant_pool[constant_pool_count -1];
+    cp_info *constant_pool; //IMPORTANTE: O Constant_Pool inicia no indice 1 !!!
+    u2 access_flags;
+    u2 this_class;
+    u2 super_class;
+    u2 interfaces_count;
+    //u2 interfaces[interfaces_count];
+    u2 *interfaces;
+    u2 fields_count;
+    field_info fields[fields_count];
+    u2 methods_count;
+    method_info methods[methods_count];
+    u2 attributes_count;
+    attribute_info attributes[attributes_count];
+}ClassFile;
+
+typedef struct{
+    u2 access_flags;  
+	u2 name_index;
+    u2 descriptor_index;
+    u2 attributes_count;   
+	//attribute_info attributes[attributes_count];
+    attribute_info *attributes;
+} field_info;
+
+typedef struct{
+
+} attribute_info;
+
+
+
+
+
--- LeitorExibidor/main.c
+++ LeitorExibidor/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "lib/macros.h"
 
-int main()
-{
+int main(){
+    u1 teste1;
+    u2 teste2;
+    u4 teste4;
     printf("Hello world!\n");
     return 0;
 }
